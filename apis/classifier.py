810c5762b1076bd2e90d7582d9b874db90e4e791
mohan <reddimohana@gmail.com>
2019-07-14 22:25:38 +0530

    updating repo with real time model so that It is more understandable

--- apis/classifier.py
+++ apis/classifier.py
@@ -17,8 +17,8 @@ from . import models as model
 Creating a parser obj, so that you can display input form in your API docs
 """
 parser = api.parser()
-parser.add_argument('sample_text', type=str, help='Help text', location='form')
-parser.add_argument('file', type=FileStorage, location='files')
+# parser.add_argument('image', type=str, help='Help text', location='form')
+parser.add_argument('image', type=FileStorage, help='Upload cat or dog image to classify.', location='files')
 
 @api.route('/')
 class ClassName(Resource):
@@ -29,25 +29,32 @@ class ClassName(Resource):
         # Add all global configuration in config.yaml so you access it through self.config
         print(self.config)
 
+
     @api.doc(parser= parser)
     def post(self):
         """
-        request.form will give you the text input parameters sent by user
-        request.files to receive files sent by user
+        request.files Choose an image to classify
         """
         print(request.form)
-        if 'sample_text' not in request.form:
-            return api.abort(400, "Input sample text required", status= "error")
+        if 'image' not in request.files:
+            return api.abort(400, "Select an image to classify", status= "error")
 
+        image_obj = request.files['image']
+        print(image_obj)
         print(self.model)
 
-        return { 'status': 'success', 'data': request.form['sample_text'] }, 200
+        return { 'status': 'success', 'prediction': 'work-in-progress' }, 200
+
+
+    def predict(self):
+        pass
+
 
     def _init_models(self):
         """
         Reading machine learning models from the memory. (We have already loaded models in app_server.py file)
         """
-        self.model = model.model_data
+        self.model = model.classifier
         self.graph = model.graph
 
 
@@ -57,6 +64,7 @@ class ClassName(Resource):
         """
         config = yaml.safe_load(open(os.path.join(self._get_cwd(),"config.yml")))
         return config
+
 
     def _get_cwd(self):
         """
